9ec5c508f2089ff7514855d5404a2db4139413ab
Thomas Fung <thomas.fung.dr@gmail.com>
2020-06-25 21:43:45 +1000

    Fixed text_graphics.R. The incorrect expectation function was used.

--- tests/testthat/test_graphics.R
+++ tests/testthat/test_graphics.R
@@ -5,7 +5,8 @@ test_that("Test gg_plot",
           { data("attendance")
             M.attendance <- glm.cmp(daysabs~ gender+math+prog, 
                                     data=attendance)
-            expect_length(gg_plot(M.attendance, which = 1:7), 7)
-            expect_warning(gg_plot(M.attendance, which=8))
+            expect_length(gg_plot(M.attendance, which = 1:8), 8)
+            expect_output(gg_plot(M.attendance, which=9), 
+                          NULL)
           })
 
